fc50c03b378ce5b2eb53237367005439741c984f
sangi-x23 <ssg5670@gmail.com>
2026-02-04 10:47:29 -0500

    cors, logger morgan and global prefix seted on main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import morgan from 'morgan';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.use(morgan('dev'));
+
+  app.setGlobalPrefix('API-STYLEMEET');
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
