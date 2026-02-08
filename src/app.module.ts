3041d793756cf61f71ddb918be97224799a44298
Santiago Sanchez Giraldo <ssg5670@gmail.com>
2026-02-08 14:26:03 -0500

    refactor: App modules imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { dbConfig } from './global/config/db/connection';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './features/users/users.module';
-import { NotificationsModule } from './features/notifications/notifications.module';
-import { ConfigsModule } from './features/configs/configs.module';
-import { PaymentsModule } from './features/payments/payments.module';
-import { AppointmentsModule } from './features/appointments/appointments.module';
-import { ClientsModule } from './features/clients/clients.module';
-import { AuthModule } from './features/auth/auth.module';
-import { RolesModule } from './features/roles/roles.module';
-import { BranchesModule } from './features/branches/branches.module';
-import { CompanysModule } from './features/companys/companys.module';
-import { UserModule } from './features/user/user.module';
-import { UsersModule } from './features/users/users.module';
+
+import * as appModules from "./features/index";
 
 @Module({
   imports: [
@@ -22,17 +12,7 @@ import { UsersModule } from './features/users/users.module';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot(dbConfig),
-    UsersModule,
-    UserModule,
-    CompanysModule,
-    BranchesModule,
-    RolesModule,
-    AuthModule,
-    ClientsModule,
-    AppointmentsModule,
-    PaymentsModule,
-    ConfigsModule,
-    NotificationsModule,
+    ...Object.values(appModules),
   ],
   controllers: [],
   providers: [],
