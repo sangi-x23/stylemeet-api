fd1a1e2968de0a370e68fb750f3a499778a5fba2
Santiago Sanchez Giraldo <santiago.sanchez@pim-bi.no>
2026-03-25 14:05:35 -0500

    fix: Migration to PostgreDB

--- src/global/config/db/connection.ts
+++ src/global/config/db/connection.ts
@@ -1,6 +1,9 @@
 import { env } from '../env/env';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+import * as fs from 'fs';
+import * as path from 'path';
+
 export const dbConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: env.DB_HOST,
@@ -9,5 +12,15 @@ export const dbConfig: TypeOrmModuleOptions = {
   password: env.DB_PASSWORD,
   database: env.DB_NAME,
   autoLoadEntities: true,
-  synchronize: true,
+  synchronize: env.NODE_ENV !== 'prod',
+  ssl: env.NODE_ENV === 'prod'
+    ? {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(path.join(__dirname, '..', 'certs', 'global-bundle.pem')).toString(),
+      }
+    : env.NODE_ENV === 'dev'
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
 };
